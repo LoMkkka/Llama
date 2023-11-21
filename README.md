10332c0ffb0b00b6f776e7e48e6a4c10c2b15985
LoMkkka <dima12671@mail.ru>
2023-11-21 05:41:23 +0000

    laala

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <h1 align="center">Развертывание LLAMA в Grafana с помощью Terrafrom и Ansible</a> 
 <img src="https://github.com/blackcater/blackcater/raw/main/images/Hi.gif" height="32"/></h1>
 
-LLAMA (Loss and Latency MAtrix) - это библиотека для тестирования и измерения сетевых потерь и задержек между распределенными конечными точками.
+LLAMA (Loss and Latency MAtrix) - это библиотека для тестирования и измерения сетевых потерь и задержек между распределенными конечными точками. Ссылка на [проект](https://github.com/dropbox/llama).
 
 Collector отправляет UDP-пакет (зонд) на прослушиваемый порт reflector и измеряет, сколько времени требуется для их возврата.
 
--- req.sh
+++ req.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+#snap install terraform
 # Запрашиваем у пользователя число
+"host.txt"
+file=`cat host.txt`
 echo "Введите число от 0 до 9:"
-read num
+read num 
+# ip=`sed -n "$num p" $file1`
+# echo $ip
 
+# for ip in $file; do 
+#     echo $ip
+#     done
 # Проверяем, что число находится в диапазоне от 0 до 9
-if [ $num -lt 0 || $num -gt 9 ]; then
+
+
+
+if [[ $num -lt 0 || $num -gt 20 ]]; then
     echo "Вы ввели неверное число. Попробуйте еще раз."
 else
     # Если число меньше или равно 0, то выводим 0 строк
@@ -12,15 +23,16 @@ else
         echo ""
     else
         # Иначе выводим строку, содержащую заданное число
-        for i in $(seq -w $num); do
-            echo "$((i + 1)) $((i + 1)) $((i + 1))"
+        for i in $(seq $num); do
+        for ip in $file; do 
+            echo -n "
+    - ip: $ip
+      port: 14000
+      tags:
+        src_hostname: Master-node
+        dst_hostname: node-$((i))"
+        done #>> complex_example.yaml
+        echo
         done
     fi
 fi
-
-
-    # - ip: 10.10.1.96 #
-    #   port: 14000
-    #   tags:
-    #     src_hostname: Master-node
-    #     dst_hostname: node-$var
