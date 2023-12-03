fa1b8cc5c119b90ed7105fa4418e8d4a46a42869
LoMkkka <76530062+LoMkkka@users.noreply.github.com>
2023-12-03 17:01:43 +0300

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -7,14 +7,14 @@ sudo apt install ansible -y
 # Установка Terraform
 sudo snap install terraform --classic 
 # Перенос утилиты 
-cp terraform-inventory /usr/local/bin
+cp /root/Llama/files/terraform-inventory /usr/local/bin
 # Генерация ssh ключа
 ssh-keygen
 # Зеркало
-cp files/.terraformrc ~/
+cp /root/Llama/files/.terraformrc ~/
 ### Передача ssh ключа в переменную для master  
 ssh_key_path=`cat ~/.ssh/id_rsa.pub`
 echo $ssh_key_path
 word="put" 
-file="./terraform/master/vars.tf"
+file="/root/Llama/terraform/master/vars.tf"
 sed -i "s#$word#$ssh_key_path#" "$file"
